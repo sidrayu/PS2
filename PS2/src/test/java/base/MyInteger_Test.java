e9a3d0bd9ab34c531baf9812e18f6b0e32054cf3
changyu <changyu@wifi-roaming-128-4-206-119.host.udel.edu>
2016-09-22 18:51:45 -0400

    Second

--- PS2/src/test/java/base/MyInteger_Test.java
+++ PS2/src/test/java/base/MyInteger_Test.java
@@ -29,6 +29,47 @@ public class MyInteger_Test {
 	@Test
 	public void test() {
 		assertTrue(1==1);
+		MyInteger numberA = new MyInteger(10);
+		MyInteger numberB = new MyInteger(9);
+		MyInteger numberC = new MyInteger(7);
+		MyInteger numberD = new MyInteger(1);
+		
+		assertTrue(numberA.isEven());
+		assertFalse(numberB.isEven());
+		assertTrue(numberB.isOdd());
+		assertFalse(numberA.isOdd());
+		assertFalse(numberA.isPrime());
+		assertTrue(numberC.isPrime());
+		assertTrue(numberD.isPrime());
+		
+		int numberE=16;
+		int numberF=15;
+		int numberG=19;
+		int numberH=1;
+		
+		assertTrue(MyInteger.isEven(numberE));
+		assertFalse(MyInteger.isEven(numberF));
+		assertTrue(MyInteger.isOdd(numberF));
+		assertFalse(MyInteger.isOdd(numberE));
+		assertTrue(MyInteger.isPrime(numberG));
+		assertFalse(MyInteger.isPrime(numberE));
+		assertTrue(MyInteger.isPrime(numberH));
+		
+		assertTrue(MyInteger.isEven(numberA));
+		assertFalse(MyInteger.isEven(numberB));
+		assertTrue(MyInteger.isOdd(numberB));
+		assertFalse(MyInteger.isOdd(numberA));
+		assertFalse(MyInteger.isPrime(numberA));
+		assertTrue(MyInteger.isPrime(numberC));
+		assertTrue(MyInteger.isPrime(numberD));
+		
+		assertTrue(numberA.equals(10));
+		assertFalse(numberA.equals(numberB));
+		
+		assertEquals(numberB.getiValue(),9);
+		
+		
+		
 	}
-
+	
 }
